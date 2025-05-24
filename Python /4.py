0854a858a264a4981db78ba33bc4c2fb93c09159
Dalip-Kumar <dalipkumar3559@gmail.com>
2025-05-24 15:57:00 +0530

    Create 4.py

---
+++ Python /4.py
@@ -0,0 +1,13 @@
+for i in range(5+1):
+    for j in range(i,5):
+        print(end=" ")
+    for k in range(1,i+1):
+        print("*",end="")
+    print()
+
+    # OUTPUT:-
+    #     *
+    #    **
+    #   ***
+    #  ****
+    # *****
