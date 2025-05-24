62280dee7a10185997d9d01fbe09a4daa964e205
Dalip-Kumar <dalipkumar3559@gmail.com>
2025-05-24 16:02:36 +0530

    Create 5.py

---
+++ Python /5.py
@@ -0,0 +1,13 @@
+for i in range(5+1):
+    for j in range(i):
+        print(end=" ")
+    for k in range(i,5):
+        print("*",end="")
+    print()
+
+    # OUTPUT:-
+    # *****    
+    #  ****
+    #   ***
+    #    **
+    #     *
