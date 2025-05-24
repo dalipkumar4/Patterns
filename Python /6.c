e80a1911f00a8f95756605700c8d8ace069ae864
Dalip-Kumar <dalipkumar3559@gmail.com>
2025-05-24 20:18:09 +0530

    Create 6.c

---
+++ Python /6.c
@@ -0,0 +1,15 @@
+for i in range(5):
+    for j in range(i,5):
+        print(" ", end="")
+    for p in range(i+1):
+        print("*",end="")
+    for k in range(i):
+        print("*",end="")
+    print()
+
+    # OUTPUT:-
+    #     *    
+    #    ***
+    #   *****
+    #  *******
+    # *********
