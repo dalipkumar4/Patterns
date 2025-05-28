95d9bc5973223d2cf2753fac8d8b0dd92d233466
Dalip-Kumar <dalipkumar3559@gmail.com>
2025-05-28 19:54:48 +0530

    Create 7.c

---
+++ C /7.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(){
+    int i,j,k,p;
+
+    for(i = 0;i<=5;i++){
+        for(j = 0;j<i;j++){
+            printf(" ");
+        }
+        for(k = i;k<=5;k++){
+            printf("*");
+        }
+        for(p = i;p<5;p++){
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
+// OUTPUT:-
+// ***********
+//  *********
+//   *******
+//    *****
+//     ***
+//      *
