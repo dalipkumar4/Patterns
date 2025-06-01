91df5112195b0012399279a2e36d823e4084bb35
Dalip-Kumar <dalipkumar3559@gmail.com>
2025-06-01 14:02:58 +0530

    Update 8.py

--- Python /8.py
+++ Python /8.py
@@ -1,14 +1,16 @@
-for i in range(5): 
-    for j in range(5): 
-        if i==0 or i==4 or j ==0 or j==4: 
-            print("*",end="") 
-        else: 
-            print(end=" ") 
+for i in range(5, -1, -1):
+    for j in range(i, 5):
+        print(" ", end="")        
+    for p in range(i + 1):
+        print("*", end="")   
+    for k in range(i):
+        print("*", end="")        
     print()
 
-# OUTPUT:-
-#  *****
-#  *   * 
-#  *   * 
-#  *   * 
-#  *****
+    # OUTPUT:-
+    # ***********
+    #  *********
+    #   *******
+    #    *****
+    #     ***
+    #      *
