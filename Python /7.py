2ebbd72f764f7d9e2536836b1eba1fa5755c6ef8
Dalip-Kumar <dalipkumar3559@gmail.com>
2025-06-01 13:57:44 +0530

    Update 7.py

--- Python /7.py
+++ Python /7.py
@@ -1,33 +1,15 @@
-# Upper half of the diamond
 for i in range(5):
-    for j in range(i, 5):
-        print(" ", end="")         # Spaces before stars
-    for p in range(i + 1):
-        print("*", end="")         # Left half of stars
-    for k in range(i):
-        print("*", end="")         # Right half of stars
+    for j in range(i,5):
+        print("-",end="")
+    for k in range(0,i+1):
+        print("*",end="")
+    for p in range(0,i):
+        print("*",end="")
     print()
-
-# Lower half of the diamond
-for i in range(5, -1, -1):
-    for j in range(i, 5):
-        print(" ", end="")         # Spaces before stars
-    for p in range(i + 1):
-        print("*", end="")         # Left half of stars
-    for k in range(i):
-        print("*", end="")         # Right half of stars
-    print()
-
-
+    
     # OUTPUT:-
-    #       *    
-    #      ***
-    #     *****
-    #    *******
-    #   *********
-    #  ***********
-    #   *********
-    #    *******
-    #     *****
-    #      ***
-    #       *
+    #     *    
+    #    ***
+    #   *****
+    #  *******
+    # *********
