a40206c5c7d04e178fa7e0358ff1b73103da2025
Dalip-Kumar <dalipkumar3559@gmail.com>
2025-06-01 13:53:17 +0530

    Create 11.py

---
+++ Python /11.py
@@ -0,0 +1,21 @@
+for i in range(5):
+    for j in range(i):
+        print("*",end="")
+    print()
+
+for i in range(5):
+    for j in range(5,i,-1):
+        print("*",end="")
+    print()
+
+
+# OUTPUT:-
+#  *
+#  **
+#  ***   
+#  ****
+#  *****
+#  ****
+#  ***
+#  **
+#  *
