2a2f8178bbd6054a02739263ba1bf7be378e23fd
Dalip-Kumar <dalipkumar3559@gmail.com>
2025-06-11 12:04:24 +0530

    Update 3.c

--- C /3.c
+++ C /3.c
@@ -1,17 +1,25 @@
-    int i, j, k;
-    for(i = 1;i<=5;i++){
-        for(j=1;j<i;j++){
+#include <stdio.h>
+
+int main()
+{
+    int i, j, p, k;
+
+    for (i = 0; i < 5; i++)
+    {
+        for (j = i; j < 5; j++)
+        {
             printf(" ");
         }
-        for(k = 1;k<=5;k++){
+        for (p = 0; p <= i; p++)
+        {
+            printf("*");
+        }
+        for (k = 0; k < i; k++)
+        {
             printf("*");
         }
         printf("\n");
     }
 
-    // OUTPUT:-
-    // *****
-    //  *****
-    //   *****
-    //    *****
-    //     *****
+    return 0;
+}
